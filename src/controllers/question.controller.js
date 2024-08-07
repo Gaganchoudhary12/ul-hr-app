508d13205798f8b945382f036b644d74fac68a31
Gagan choudhary <55543086+Gaganchoudhary12@users.noreply.github.com>
2024-08-07 13:29:34 +0530

    Update question.controller.js

--- src/controllers/question.controller.js
+++ src/controllers/question.controller.js
@@ -1,6 +1,6 @@
 import Questions from '../models/question.js'
 
 export const getQuestions = async (req, res) => {
-  const questions = await Questions.find({},{_id:0});
+  const questions = await Questions.find({type:{$exists:false}},{_id:0});
   res.status(200).send(questions);
 };
