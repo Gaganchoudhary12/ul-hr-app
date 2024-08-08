3c3bd2929cd9be5a46b13c7bddeedc6bdea63f9e
Gagan choudhary <55543086+Gaganchoudhary12@users.noreply.github.com>
2024-08-08 14:20:30 +0530

    Delete src directory

--- src/controllers/question.controller.js
+++
@@ -1,6 +0,0 @@
-import Questions from '../models/question.js'
-
-export const getQuestions = async (req, res) => {
-  const questions = await Questions.find({type:{$exists:false}},{_id:0});
-  res.status(200).send(questions);
-};
--- src/models/feedback.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const SchemaCommon = new mongoose.Schema(
-  {},
-  { strict: false, versionKey: false }
-);
-const Feedback = mongoose.model("feedback", SchemaCommon, "feedback");
-
-export default Feedback;
