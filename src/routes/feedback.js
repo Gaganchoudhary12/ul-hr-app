93231e3e14d59135d1a49b12ecd71fe9b0ca2242
Gagan choudhary <55543086+Gaganchoudhary12@users.noreply.github.com>
2024-08-21 18:36:39 +0530

    Delete src directory

--- src/routes/feedback.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getFeedbackQuestion, submitFeedback } from '../controllers/feedback.controller.js'
-
-const feedbackRouter = express.Router();
-
-feedbackRouter.post('/', submitFeedback);
-feedbackRouter.get('/question', getFeedbackQuestion);
-
-export default feedbackRouter
