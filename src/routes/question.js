4b35c13bc70cb5f21ec4a9686183b42a00d6fa00
Gagan choudhary <55543086+Gaganchoudhary12@users.noreply.github.com>
2024-08-21 18:33:20 +0530

    Delete src directory

--- src/routes/question.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getQuestions } from "../controllers/question.controller.js";
-
-const questionRouter = express.Router();
-
-questionRouter.get("/", getQuestions);
-
-export default questionRouter;
