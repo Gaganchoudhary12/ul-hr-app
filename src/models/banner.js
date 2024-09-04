25f48485250ffc8d2effd4bd06ddf241b1df3ed7
Gagan choudhary <55543086+Gaganchoudhary12@users.noreply.github.com>
2024-09-04 13:19:32 +0530

    Delete src directory

--- src/models/banner.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const SchemaCommon = new mongoose.Schema(
-  {},
-  { strict: false, versionKey: false }
-);
-const Banner = mongoose.model("banners", SchemaCommon, "banners");
-
-export default Banner;
