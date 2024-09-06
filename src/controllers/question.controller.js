97665e8ddcc1ddaf2f10c8b890692fa839408242
Gagan choudhary <55543086+Gaganchoudhary12@users.noreply.github.com>
2024-09-06 10:57:37 +0530

    Update question.controller.js

--- src/controllers/question.controller.js
+++ src/controllers/question.controller.js
@@ -1,6 +1,14 @@
-import Questions from '../models/question.js'
-
+import Questions from "../models/question.js";
+ 
 export const getQuestions = async (req, res) => {
-  const questions = await Questions.find({type:{$exists:false}},{_id:0});
-  res.status(200).send(questions);
+  const totalTitles = await Questions.distinct("title");
+  const questions = [];
+  for (const key of totalTitles) {
+    const data = await Questions.find({
+      title:key,
+    }, { _id: 0 });
+    questions.push({ title: key, questions: data });
+  }
+ 
+  res.status(200).send(questions.reverse());
 };
